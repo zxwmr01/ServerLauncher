ab4b7397a2fe8906e94068c9e5840a5ce927ea98
才羽青空 <xiaohuo_6@qq.com>
2025-03-21 13:15:27 +0800

    Update Chinese Version

---
+++ ServerLauncher/AboutWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace ServerLauncher
+{
+    /// <summary>
+    /// Window2.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        //加入 Discord 按钮
+        private void Button_JoinDC_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://discord.gg/yrcaukyEcw";
+            try
+            {
+                // 使用默认浏览器打开链接
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (System.ComponentModel.Win32Exception ex)
+            {
+                // 如果系统中没有默认浏览器，会抛出异常
+                MessageBox.Show($"无法打开链接：{ex.Message}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        //加入 QQ 群 按钮
+        private void Button_JoinQQ_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://qm.qq.com/q/q8dzw0E8Xm";
+            try
+            {
+                // 使用默认浏览器打开链接
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (System.ComponentModel.Win32Exception ex)
+            {
+                // 如果系统中没有默认浏览器，会抛出异常
+                MessageBox.Show($"无法打开链接：{ex.Message}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ ServerLauncher/Window1.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace ServerLauncher
+{
+    public partial class NewWindow : Window
+    {
+        private DispatcherTimer timer;
+
+        public NewWindow()
+        {
+            InitializeComponent();
+
+            // 初始化定时器
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(5); // 每 5 秒检测一次
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            // 初始检测
+            CheckTslGameProcess();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // 定时检测进程状态
+            CheckTslGameProcess();
+        }
+
+        private void CheckTslGameProcess()
+        {
+            // 获取所有名为 "TslGame" 的进程
+            Process[] processes = Process.GetProcessesByName("TslGame");
+
+            if (processes.Length > 0)
+            {
+                // 如果进程存在，更新界面为“运行”，并设置为绿色
+                StatusText.Text = "运行";
+                StatusText.Foreground = new SolidColorBrush(Colors.Green);
+            }
+            else
+            {
+                // 如果进程不存在，更新界面为“未运行”，并设置为红色
+                StatusText.Text = "未运行";
+                StatusText.Foreground = new SolidColorBrush(Colors.Red);
+            }
+        }
+    }
+}
